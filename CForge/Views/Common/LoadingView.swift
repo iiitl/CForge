3b6bf1d489d32ce3755d7b14ea9af08cf20e0b1c
kpflow369 <khushipatil187@gmail.com>
2026-04-10 12:46:00 +0530

    FIXED #9 issue

--- CForge/Views/Common/LoadingView.swift
+++ CForge/Views/Common/LoadingView.swift
@@ -3,12 +3,14 @@ struct LoadingView: View {
     var body: some View {
         VStack {
             ProgressView()
-                .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+                .progressViewStyle(CircularProgressViewStyle(tint: .neonBlue))
                 .scaleEffect(1.5)
             Text("Loading...")
                 .padding(.top, 8)
+                .foregroundColor(.textSecondary)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.darkBackground)
     }
 }
 #Preview {
